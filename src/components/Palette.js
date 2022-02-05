77886f6a197c5f23b89eed62c19d23b242916826
shlomohal84 <stairwaytoheaven84@gmail.com>
2022-02-05 15:23:28 +0200

    creating project

---
+++ src/components/Palette.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Palette extends Component {
+	render() {
+		return (
+			<div className='Pallete'>
+				{/* Navbar goes here */}
+				<div className='Palette-colors'>	{/* Bunch of color boxes */}</div>
+				{/* Footer  */}
+			</div>
+		);
+	}
+}
