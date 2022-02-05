a365df2fdcaeb7fa7303287ebaec3e1da548af24
shlomohal84 <stairwaytoheaven84@gmail.com>
2022-02-05 15:23:28 +0200

    creating project

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import "./App.css";
+
+import seedColors from "./seedColors";
+import Palette from "./components/Palette";
+
+
+export default class App extends Component {
+
+
+  render() {
+
+    return (
+      <div className="App">
+        <Palette palette={seedColors[4]} />
+      </div>
+    );
+  }
+}
