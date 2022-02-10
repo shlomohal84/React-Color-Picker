2431ccfb18fd03263e4abdd2de9d62a5428a07ca
shlomohal84 <stairwaytoheaven84@gmail.com>
2022-02-10 22:13:54 +0200

    Add Select and Material UI

--- src/components/Palette.js
+++ src/components/Palette.js
@@ -7,24 +7,34 @@ import "./Palette.css";
 export default class Palette extends Component {
 	constructor(props) {
 		super(props);
-		this.state = { level: 500 };
-		this.changeLevel = this.changeLevel.bind(this)
+		this.state = { level: 500, format: "hex" };
+		this.changeLevel = this.changeLevel.bind(this);
+		this.changeFormat = this.changeFormat.bind(this);
 	}
 	changeLevel(level) {
-		this.setState({ level })
+		this.setState({ level });
 	}
-
+	changeFormat(val) {
+		this.setState({ format: val });
+	}
 	render() {
 		const { colors } = this.props.palette;
-		const { level } = this.state
-		const colorBoxes = colors[level].map(
-			(color) => (
-				<ColorBox key={color.name} background={color.hex} name={color.name} changeLevel={this.changeLevel} />
-			)
-		);
+		const { level, format } = this.state;
+		const colorBoxes = colors[level].map((color) => (
+			<ColorBox
+				key={color.name}
+				background={color[format]}
+				name={color.name}
+				changeLevel={this.changeLevel}
+			/>
+		));
 		return (
 			<div className="Palette">
-				<Navbar level={level} changeLevel={this.changeLevel} />
+				<Navbar
+					level={level}
+					changeLevel={this.changeLevel}
+					handleChange={this.changeFormat}
+				/>
 				<div className="Palette-colors">{colorBoxes}</div>
 				{/* Footer  */}
 			</div>
