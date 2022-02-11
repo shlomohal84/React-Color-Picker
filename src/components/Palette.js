e0805739ec3654b2b08c85f4893365d10767e4f3
shlomohal84 <stairwaytoheaven84@gmail.com>
2022-02-11 13:47:08 +0200

    Add Snackbar and styled Select

--- src/components/Palette.js
+++ src/components/Palette.js
@@ -1,43 +1,43 @@
-import React, { Component } from "react";
+import React, { Component } from "react"
 
-import Navbar from "./Navbar";
-import ColorBox from "./ColorBox";
+import Navbar from "./Navbar"
+import ColorBox from "./ColorBox"
 
-import "./Palette.css";
+import "./Palette.css"
 export default class Palette extends Component {
-	constructor(props) {
-		super(props);
-		this.state = { level: 500, format: "hex" };
-		this.changeLevel = this.changeLevel.bind(this);
-		this.changeFormat = this.changeFormat.bind(this);
-	}
-	changeLevel(level) {
-		this.setState({ level });
-	}
-	changeFormat(val) {
-		this.setState({ format: val });
-	}
-	render() {
-		const { colors } = this.props.palette;
-		const { level, format } = this.state;
-		const colorBoxes = colors[level].map((color) => (
-			<ColorBox
-				key={color.name}
-				background={color[format]}
-				name={color.name}
-				changeLevel={this.changeLevel}
-			/>
-		));
-		return (
-			<div className="Palette">
-				<Navbar
-					level={level}
-					changeLevel={this.changeLevel}
-					handleChange={this.changeFormat}
-				/>
-				<div className="Palette-colors">{colorBoxes}</div>
-				{/* Footer  */}
-			</div>
-		);
-	}
+  constructor(props) {
+    super(props)
+    this.state = { level: 500, format: "hex" }
+    this.changeLevel = this.changeLevel.bind(this)
+    this.changeFormat = this.changeFormat.bind(this)
+  }
+  changeLevel(level) {
+    this.setState({ level })
+  }
+  changeFormat(val) {
+    this.setState({ format: val })
+  }
+  render() {
+    const { colors } = this.props.palette
+    const { level, format } = this.state
+    const colorBoxes = colors[level].map((color) => (
+      <ColorBox
+        key={color.name}
+        background={color[format]}
+        name={color.name}
+        changeLevel={this.changeLevel}
+      />
+    ))
+    return (
+      <div className="Palette">
+        <Navbar
+          level={level}
+          changeLevel={this.changeLevel}
+          handleChange={this.changeFormat}
+        />
+        <div className="Palette-colors">{colorBoxes}</div>
+        {/* Footer  */}
+      </div>
+    )
+  }
 }
