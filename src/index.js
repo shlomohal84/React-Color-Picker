28c6350da6bf1199ac5633fc7e4d525052b0d1b5
shlomohal84 <stairwaytoheaven84@gmail.com>
2022-02-11 13:47:23 +0200

    Disable StrictMode

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
-import React, { StrictMode } from "react"
-import { render } from "react-dom"
-import { BrowserRouter } from "react-router-dom"
-import App from "./App"
+import React /* , { StrictMode } */ from "react";
+import { render } from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 
 render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>
-  ,
+  // <StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  // </StrictMode>
   document.getElementById("root")
-)
+);
