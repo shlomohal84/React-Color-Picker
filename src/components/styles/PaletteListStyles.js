c95f75224ad17e9d681713aa136e92cce4212c4e
shlomohal84 <stairwaytoheaven84@gmail.com>
2022-02-23 18:19:37 +0200

    Add PaletteList background svg

--- src/components/styles/PaletteListStyles.js
+++ src/components/styles/PaletteListStyles.js
@@ -1,12 +1,20 @@
 import sizes from "./sizes";
-
+import bg from "./bg.svg";
 const PaletteListStyles = {
   root: {
-    backgroundColor: "blue",
-    height: "100vh",
+    minHeight: "100vh",
     display: "flex",
     alignItems: "flex-start",
     justifyContent: "center",
+    /* background by SVGBackgrounds.com */
+    backgroundColor: "#394BAD",
+    backgroundImage: `url(${bg})`,
+    /* background by SVGBackgrounds.com */
+    overFlow: "scroll",
+    paddingBottom: "3rem",
+  },
+  heading: {
+    fontSize: "2rem",
   },
   container: {
     width: "50%",
@@ -42,7 +50,7 @@ const PaletteListStyles = {
     },
     [sizes.down("xs")]: {
       gridTemplateColumns: "repeat(1,100%)",
-      gridGap: "1rem",
+      gridGap: "1.4rem",
     },
   },
 };
